10cad81dad5d3ed50b384f1e511bf23ac6285934
amp6667 <amp6667@rit.edu>
2015-10-25 18:32:56 -0400

    added basic php connection

--- connect.php
+++ connect.php
@@ -1,2 +1,19 @@
-#Php script
-#php code goes here
+<html>
+<head>
+<title>Connecting MySQL Server</title>
+</head>
+<body>
+<?php
+   $dbhost = 'mydbinstance.caaxufewczs3.us-east-1.rds.amazonaws.com:3036';
+   $dbuser = 'awsuser';
+   $dbpass = 'iste43201';
+   $conn = mysql_connect($dbhost, $dbuser, $dbpass);
+   if(! $conn )
+   {
+     die('Could not connect: ' . mysql_error());
+   }
+   echo 'Connected successfully';
+   mysql_close($conn);
+?>
+</body>
+</html>
