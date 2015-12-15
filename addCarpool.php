6e9eb146aba2db0e6fda778437aa2741fa5a2688
amp6667 <amp6667@rit.edu>
2015-12-15 11:56:07 -0500

    Added carpool

---
+++ addCarpool.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+	<title>Add Carpool</title>
+</head>
+<body>
+
+<script src=></script>
+<script>
+	function getLongLat(){
+		var autocomplete = new.maps.places.Autocomplete(document.getElementById());
+	}
+</script>
+<?php>
+
+
+?>
+
+</body>
+</html>
